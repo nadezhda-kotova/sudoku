c9ae0241b1fa35c80b611e621a8fbcde4e5c7170
unknown <nadezhda.1.kotova@gmail.com>
2019-02-24 22:49:39 +0300

    Resolved task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  function numbers(row, col) {
+    let position = {
+      row: Math.floor(row / 3) * 3,
+      col: Math.floor(col / 3) * 3
+    };
+    let result = [];
+    for (i = 0; i < 9; i++) {
+      result.push([matrix[row][i], matrix[i][col], matrix[position.row + i % 3][position.col + Math.floor(i / 3)]])
+    }
+    return result;
+  }
+  for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++) {
+      if (matrix[row][col] == 0) {
+        let number = numbers(row, col);
+        for (let num of number) {
+          matrix[row][col] = num;
+        }
+      }
+    }
+  }
+  return matrix;
 }
